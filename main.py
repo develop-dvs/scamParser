7cbcb9eadef80535fb7c29e579e7a34fb9bba70b
Develop <2734059+develop-dvs@users.noreply.github.com>
2022-05-18 14:04:05 +0300

    Update main.py
    new format (csv)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
+import csv
 import threading
 import time
 
@@ -7,44 +7,57 @@ from tqdm import tqdm
 
 OUTPUT_DIR = "data"
 THREADS_COUNT = 100
-BASE_URL = "https://saverudata.org/dbgeo/"
+BASE_URL = "https://saverudata.info/"
 
 
-def fetch_json(url):
+def fetch_csv(url):
     i = 0
     while i < 3:
         try:
             res = requests.get(url)
-            return res.json()
-        except Exception:
+            return res.content.decode('utf-8')
+        except Exception as err:
+            print(f"Error to fetch  {url} {err=}, {type(err)=}")
             i += 1
             time.sleep(1)
     return f"Error to fetch {url}"
 
 
-def get_json_data(file):
+def get_csv_data(file):
     with open(file) as geohash:
-        data = json.load(geohash)
+        reader = csv.reader(geohash, delimiter=',')
+        data = list(reader)
         chunk = len(data) // THREADS_COUNT
         return list(zip(*[iter(data)] * chunk))
-
 
 def handle_chunk(pos, chunk):
     text = f"Fetch #{pos}"
     with tqdm(total=len(chunk), position=pos, desc=text) as progress:
         for g_str in chunk:
-            url = f"{BASE_URL}{g_str[0:1]}/{g_str[1:2]}/{g_str[2:3]}/{g_str}.json"
-            with open(f"{OUTPUT_DIR}/{g_str}.json", "w+", encoding="utf-8") as out:
+            # /db/dbgeo/s/z/g/szgcye.csv
+            url = f"{BASE_URL}db/dbgeo/{g_str[0][0:1]}/{g_str[0][1:2]}/{g_str[0][2:3]}/{g_str[0]}.csv"
+            with open(f"{OUTPUT_DIR}/{g_str[0]}.csv", "w+", encoding="utf-8") as out:
                 try:
-                    response = fetch_json(url)
-                    out.write(json.dumps(response, ensure_ascii=False))
+                    response = fetch_csv(url)
+                    out.write(response)
                 except Exception:
                     out.write(f"Error to fetch {url}")
             progress.update(1)
 
 
 def main():
-    data = get_json_data("geohash.json")
+    # download latest geohash base
+    url = f"{BASE_URL}map/geohash.csv"
+    with open(f"geohash.csv", "w+", encoding="utf-8") as out:
+        try:
+            response = fetch_csv(url)
+            out.write(response)
+        except Exception as err:
+            print(f"Error to fetch  {url} {err=}, {type(err)=}")
+            out.write(f"Error to fetch {url}")
+
+    # open saved csv
+    data = get_csv_data("geohash.csv")
 
     for pos, chunk in enumerate(data, 1):
         try:
